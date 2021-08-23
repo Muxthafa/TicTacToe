e786f9858c9904713c2ab901b2319bd5d0906f15
Mohammad Musthafa <musthafamohd0@gmail.com>
2021-08-23 09:05:29 +0530

    added

---
+++ TicTacToe.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] num = new char[10];
+		for(int i=1;i<10;i++)
+		{
+			num[i]=' ';
+		}
+		for(int i=1;i<10;i++)
+		{
+			System.out.println("space : "+num[i]);
+		}
+	}
+
+}
