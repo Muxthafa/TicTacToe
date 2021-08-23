0cee7a40bcc3ec34cdb7e8bed5dc8633e56af67b
Mohammad Musthafa <musthafamohd0@gmail.com>
2021-08-23 09:06:23 +0530

    Added:Use case 1 tic tac toe

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,18 +1,20 @@
 package com.bridgelabz;
 
 public class TicTacToe {
-
+	
+	TicTacToe()		//constructor
+	{
+		System.out.println("welcome to java workshop!!");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		char[] num = new char[10];
-		for(int i=1;i<10;i++)
+		for(int i=1;i<num.length;i++)
 		{
 			num[i]=' ';
 		}
-		for(int i=1;i<10;i++)
-		{
-			System.out.println("space : "+num[i]);
-		}
+		TicTacToe board=new TicTacToe();	//object creation
 	}
 
 }
