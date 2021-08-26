ad843662f9fbc132d3d52e51782c384405e4db8e
Mohammad Musthafa <musthafamohd0@gmail.com>
2021-08-26 19:23:16 +0530

    Use case 7 to determine the winner

--- TicTacToe.java
+++ TicTacToe.java
@@ -17,6 +17,10 @@ public class TicTacToe {
 	
 	static char[] board = new char[10];		//char array with size 10
 	
+	public static String winner="no"; 
+	
+	public static int lock ;
+	
 	/*@method createBoard to initialize array
 	 * This function corresponds to Use case 1
 	 * Use case to create a new board game and initialize the values to ' '
@@ -28,7 +32,7 @@ public class TicTacToe {
 		}
 	}
 	
-	/*@method doToss identifies who to make the move first
+	/*@method doToss identifies who to selects move first
 	 * player chooses head or tail
 	 * checked against the toss output
 	 */
@@ -43,7 +47,6 @@ public class TicTacToe {
 		{
 			System.out.println("Its player's turn!!!");
 			who='p';
-			
 		}
 		else
 		{
@@ -62,16 +65,28 @@ public class TicTacToe {
 		{
 			System.out.println("Enter the input either X or O: ");
 			letter=sc.next().charAt(0);
+			lock=1;							// lock the letter chosen by player
 		}
 		else
 		{
-			if (rand.nextInt(2)==0)			//method to get number 0 or 1
-				letter='X';
-			else
-				letter='O';
+			if(lock != 1) {					//if the letter is not locked computer chooses randomly
+				if (rand.nextInt(2)==0)	{		//method to get number 0 or 1
+					letter='X';					// 0 corresponds to X
+				}
+				else {
+					letter='O';					//1 corresponds to O
+					}
+				}
+			else if(letter == 'X'){
+				letter = 'O';
+			}
+			else {
+				letter = 'X';
+			}
+			lock=1;
 		}
-		
 	}
+		
 	
 	/*@method displayBoard to display the board in cell format
 	 * this is use case 3 which allows player to make a move
@@ -132,15 +147,106 @@ public class TicTacToe {
 		displayBoard();
 	}
 	
+	
+	/*@method to check for winner
+	 * checks different cases to determine the winner
+	 * if there is a "XXX" OR "OOO" 
+	 * checked horizontally, vertically and diagonally
+	 */
+	public static void winnerExists()
+    {
+		
+		//to check if there is an winning situation
+        for (int a = 1; a < 9; a++) 
+        {
+            String match = null;
+  
+            switch (a) {
+            case 1:
+                match = ""+board[1] + board[2] + board[3];
+                break;
+            case 2:
+                match = ""+board[4] + board[5] + board[6];
+                break;
+            case 3:
+                match = ""+board[7] + board[8] + board[9];
+                break;
+            case 4:
+                match = ""+board[1] + board[4] + board[7];
+                break;
+            case 5:
+                match = ""+board[2] + board[5] + board[8];
+                break;
+            case 6:
+                match = ""+board[3] + board[5] + board[9];
+                break;
+            case 7:
+                match = ""+board[1] + board[5] + board[9];
+                break;
+            case 8:
+                match = ""+board[3] + board[5] + board[7];
+                break;
+            }
+            //For  winner
+            if (match.equals("XXX")||match.equals("OOO")) {
+                winner="yes";
+            }
+        }
+        int i;
+        for(i=1;i<board.length;i++)
+        {
+        	if(board[i]==' ')
+        		break;
+        }
+        if(i==10)
+        	winner="draw";
+        if(winner=="yes")						//condition to check for winner
+		{
+			System.out.println("Winner is: "+winnerIs());
+		}
+		else if(winner=="draw")								//condition to check for tie
+		{
+			System.out.println( "It's a draw!");
+		}
+		else													//if none of conditions determine the winner then change the turn to play
+		{
+			if(who=='c')
+			{
+				who='p';
+				System.out.println("Players turn to play");
+			}
+			else
+			{
+				who='c';
+				System.out.println("Computers turn to play");
+			}		
+		}
+    }
+	
+	//@method to return the winner
+	public static String winnerIs() {
+		if(who=='c')
+			return "Computer";
+		else
+			return "Player";
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int start = 0;
+		while(winner != "yes" || winner != "draw") {
+			if(start ==0) {
 		createBoard();	//function call to createBoard
 		doToss();		//function call to do a toss
+		start = 1;
+			}
 		getInput();   //function call as to enter either 'X' or 'O'
 		displayBoard(); //function call to display board
 		System.out.println("Enter the cell number (1 to 9) to make the move: ");
 		setLocation();	//function call to select the index value to store X OR O
 		checkSpace(); 	//function call to check for valid space
+		winnerExists();							//@method to check if someone won
+		}
 	}
 
 }
